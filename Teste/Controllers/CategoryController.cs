ab071885aa2af7e411353978999a4de41a4226e1
Eduardo_AVS <eavsilva@sga.pucminas.br>
2024-09-09 17:48:11 -0300

    Criar categoria iniciado

--- Teste/Controllers/CategoryController.cs
+++ Teste/Controllers/CategoryController.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Teste.Data;
+using Teste.Models;
 
 namespace Teste.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        
         public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
