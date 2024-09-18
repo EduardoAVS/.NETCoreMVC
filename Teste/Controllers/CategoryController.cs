41f47525107d8452eacae0c9d5ee02d6093912f1
Eduardo_AVS <eavsilva@sga.pucminas.br>
2024-09-18 14:37:27 -0300

    Crud Categorias Atualizado

--- Teste/Controllers/CategoryController.cs
+++ Teste/Controllers/CategoryController.cs
@@ -17,10 +17,56 @@ namespace Teste.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
-
         public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category"); // Já redireciona para o Controller atual automaticamente
+                                                              // mas eu deixei explícito
+            }
+            return View();
+
+        }
+        public IActionResult Edit(int? Id) // Só funcionou quando o nome passado por parâmetro foi igual asp-route-nome
+        {
+            if(Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(Id); // Só funciona na pk
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==categoryId); // Funciona em outros atributos
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id == categoryId).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category"); // Já redireciona para o Controller atual automaticamente
+                                                              // mas eu deixei explícito
+            }
+            return View();
+
+        }
     }
+
+
 }
--- Teste/Models/Category.cs
+++ Teste/Models/Category.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Teste.Models
 {
     public class Category
     {
         public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
+        public required string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
