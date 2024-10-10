5ac6587054f4c2ec9b74b2a6736765d979ffae07
Eduardo_AVS <eavsilva@sga.pucminas.br>
2024-10-10 20:56:20 -0300

    Remover categoria e Notificações temporárias adicionadas

--- Teste/Controllers/CategoryController.cs
+++ Teste/Controllers/CategoryController.cs
@@ -29,6 +29,7 @@ namespace Teste.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created sucessfully";
                 return RedirectToAction("Index", "Category"); // Já redireciona para o Controller atual automaticamente
                                                               // mas eu deixei explícito
             }
@@ -54,16 +55,49 @@ namespace Teste.Controllers
         }
 
         [HttpPost]
-        public IActionResult Edit(Category obj)
+        public IActionResult Edit(Category obj) // Se no lugar de update, criar. Verificar se o id está em obj e verificar a hidden property no html
         {
             if (ModelState.IsValid)
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated sucessfully";
                 return RedirectToAction("Index", "Category"); // Já redireciona para o Controller atual automaticamente
                                                               // mas eu deixei explícito
             }
             return View();
+
+        }
+
+        public IActionResult Delete(int? id) // Só funcionou quando o nome passado por parâmetro foi igual asp-route-nome
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category? categoryFromDb = _db.Categories.Find(id); // Só funciona na pk
+            //Category? categoryFromDb1 = _db.Categories.FirstOrDefault(u=>u.Id==categoryId); // Funciona em outros atributos
+            //Category? categoryFromDb2 = _db.Categories.Where(u=>u.Id == categoryId).FirstOrDefault();
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id) // Se no lugar de update, criar. Verificar se o id está em obj e verificar a hidden property no html
+        {
+            Category? obj = _db.Categories.Find(id);
+            if (obj == null) {
+                return NotFound();
+            }
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted sucessfully";
+            return RedirectToAction("Index");
 
         }
     }
